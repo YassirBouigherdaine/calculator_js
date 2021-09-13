6b510860850069a998a7ec3de739ebfa13bdc8f5
YassirBouigherdaine <bouigherdaineyassir@gmail.com>
2021-09-13 23:04:09 +0000

    first commit

---
+++ myApp.js
@@ -0,0 +1,34 @@
+﻿const hist = document.getElementById('hist');
+const res = document.getElementById('res');
+const keyboard = Array.from(document.querySelectorAll('[data-button]'));
+
+keyboard.map(button => {
+
+    button.onclick = (e) => {
+
+        switch (e.target.textContent) {
+            case 'CE':
+                hist.textContent = '';
+                res.textContent = '';
+                break;
+            case '=':
+                if (hist.textContent == '' && res.textContent != '') {
+
+                    hist.textContent = res.textContent;
+                    res.textContent = eval(hist.textContent);
+                }
+                else {
+                    alert("Enter new numbers");
+                }
+                break;
+            case 'C':
+                if (res.textContent) {
+                    hist.textContent = '';
+                    res.textContent = res.textContent.slice(0, -1);
+                }
+                break;
+            default:
+                res.textContent += e.target.textContent;
+        }
+    };
+});
